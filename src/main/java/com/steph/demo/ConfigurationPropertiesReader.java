935a229f22e7ad75f83b61c2af0b36455ec2c911
steph <stephane.mg@gmail.com>
2023-02-15 07:07:34 +0100

    init

--- src/main/java/com/steph/demo/ConfigurationPropertiesReader.java
+++ src/main/java/com/steph/demo/ConfigurationPropertiesReader.java
@@ -13,26 +13,35 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
+
 @Component
 public class ConfigurationPropertiesReader {
 
-  public static final String CONFIGURATION_PROPERTIES = "configuration.properties";
+    public static final String CONFIGURATION_PROPERTIES = "configuration.properties";
 
-  @Autowired
-  private Environment env;
+    @Autowired
+    private Environment env;
 
-  public Properties readProperties() {
-    Properties props =  asProperties(CONFIGURATION_PROPERTIES);
-    return props;
-  }
+    public Properties readProperties() {
+        Properties props = asProperties(CONFIGURATION_PROPERTIES);
+        return props;
+    }
 
-  private Properties asProperties(String fileName) {
-    String PREFIX = "CONSTANTE";
-    Predicate<PropertySource> isEnumerablePropertySource = propertySource -> propertySource instanceof EnumerablePropertySource;
-    Properties props = new Properties();
+    private Properties asProperties(String fileName) {
+        String PREFIX = "CONSTANTE";
+        Predicate<PropertySource> isEnumerablePropertySource = propertySource -> propertySource instanceof EnumerablePropertySource;
+        Properties props = new Properties();
+        StreamSupport.stream(
+                        ((AbstractEnvironment) env).getPropertySources().spliterator(), false)
+                .filter(isEnumerablePropertySource)
+                .map(propertySource -> (EnumerablePropertySource) propertySource)
+                .map(enumerablePropertySource -> Arrays.stream(enumerablePropertySource.getPropertyNames())
+                        .filter(p -> (enumerablePropertySource.getProperty(p) instanceof String))
+                        .filter(p -> ((String) enumerablePropertySource.getProperty(p)).startsWith(PREFIX))
+                        .collect(Collectors.toMap(Function.identity(), p -> props.setProperty(p, env.getProperty(p)))));
 //    StreamSupport.stream(
-//      ((AbstractEnvironment) env).getPropertySources().spliterator(), false)
-//      .filter(isEnumerablePropertySource)
+//                    ((AbstractEnvironment) env).getPropertySources().spliterator(), false)
+//            .filter(isEnumerablePropertySource)
 //            .forEach(propertySource -> {
 //              EnumerablePropertySource enumerablePropertySource = (EnumerablePropertySource) propertySource;
 //              Map<String, Object> propertyOverrides = Arrays.stream(enumerablePropertySource.getPropertyNames())
@@ -40,16 +49,6 @@ public class ConfigurationPropertiesReader {
 //                      .filter(p -> ((String) enumerablePropertySource.getProperty(p)).startsWith(PREFIX))
 //                      .collect(Collectors.toMap(Function.identity(), p -> props.setProperty(p, env.getProperty(p))));
 //            });
-    StreamSupport.stream(
-                    ((AbstractEnvironment) env).getPropertySources().spliterator(), false)
-            .filter(isEnumerablePropertySource)
-            .forEach(propertySource -> {
-              EnumerablePropertySource enumerablePropertySource = (EnumerablePropertySource) propertySource;
-              Map<String, Object> propertyOverrides = Arrays.stream(enumerablePropertySource.getPropertyNames())
-                      .filter(p -> (enumerablePropertySource.getProperty(p) instanceof String))
-                      .filter(p -> ((String) enumerablePropertySource.getProperty(p)).startsWith(PREFIX))
-                      .collect(Collectors.toMap(Function.identity(), p -> props.setProperty(p, env.getProperty(p))));
-            });
-    return props;
-  }
+        return props;
+    }
 }
